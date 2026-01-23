0ade47286a79a38608da62c05435ac8910dae435
Yana Karpliuk <yana.karpliuk@lembergsolutions.com>
2026-01-23 17:37:56 +0200

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint';
+import stylelint from "vite-plugin-stylelint";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    eslint(),
+    stylelint()
+  ],
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       additionalData:
+  //           '@use "/src/scss/main.scss" as *;',
+  //     },
+  //   },
+  // },
+})
